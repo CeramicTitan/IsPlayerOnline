513171e6f785892994bffd7e49051b9a72aad628
CeramicTitan <ceramictitandev@gmail.com>
2016-06-21 21:57:16 -0600

    Last Seen functionality only available if player is offline

--- src/me/ceramictitan/playeronline/listeners/SignListener.java
+++ src/me/ceramictitan/playeronline/listeners/SignListener.java
@@ -57,10 +57,10 @@ public class SignListener implements Listener {
             if (e.getClickedBlock().getType() == Material.WALL_SIGN || e.getClickedBlock().getType() == Material.SIGN_POST || e.getClickedBlock().getType() == Material.SIGN) {
                 Player player = e.getPlayer();
                 Sign sign = (Sign) e.getClickedBlock().getState();
-                if(sign.getLine(0).equalsIgnoreCase("[online]")){
+                if(sign.getLine(0).equalsIgnoreCase("[online]") ){
                     Player target = Bukkit.getPlayer(sign.getLine(1));
                     String lastSeen = plugin.getPlayerData().getString(target.getUniqueId().toString());
-                    if(lastSeen != null){
+                    if(lastSeen != null && !target.isOnline()){
                         player.sendMessage(target.getDisplayName() +" was last seen: "+ lastSeen);
                     }
                 }
